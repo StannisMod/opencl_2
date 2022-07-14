b182cd2ca7da348bad7eb8bb79dd96b42f304e0d
StannisMod <stas.batalenkov@mail.ru>
2022-07-14 11:39:38 +0300

    Added things for public release

--- main.c
+++ main.c
@@ -31,7 +31,7 @@ int main(int argc, char **argv) {
     }
 
 #if GENERATE
-    if (generate(10000000) != 0) {
+    if (generate(16000000) != 0) {
         return 1;
     }
 #endif
@@ -102,7 +102,7 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    const int batchSize = 1;
+    const int batchSize = 256;
 
     //printf("Batch size: %i\n", batchSize);
 
